85c30f6e9141f1b399866f5ef87e0204b81027b9
mferiyal <44905071+mferiyal@users.noreply.github.com>
2018-11-29 09:46:59 +0700

    a1
    a

---
+++ app/src/main/java/com/mfir/pc/ta/ScanQRCode.java
@@ -0,0 +1,18 @@
+package com.mfir.pc.ta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.util.Log;
+/*import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;*/
+
+public class ScanQRCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qrcode);
+    }
+}
