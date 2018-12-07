40eee68cef915c5968f9da790953263f57e20314
FaisalApr <45162241+FaisalApr@users.noreply.github.com>
2018-12-07 15:50:19 +0700

    activity

---
+++ app/src/main/java/com/mfir/pc/ta/user/HomeMahasiswa.java
@@ -0,0 +1,13 @@
+package com.mfir.pc.ta.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeMahasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_mahasiswa);
+    }
+}
