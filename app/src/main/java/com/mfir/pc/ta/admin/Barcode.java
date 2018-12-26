de5f59125f58447457423aae58ae26f9a6d55175
mferiyal <44905071+mferiyal@users.noreply.github.com>
2018-12-26 12:50:35 +0700

    barcode generator
    a

--- app/src/main/java/com/mfir/pc/ta/admin/Barcode.java
+++ app/src/main/java/com/mfir/pc/ta/admin/Barcode.java
@@ -1,54 +1,56 @@
 package com.mfir.pc.ta.admin;
 
+
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-import com.budiyev.android.codescanner.CodeScanner;
-import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
-import com.google.zxing.Result;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.mfir.pc.ta.R;
 
 import androidx.annotation.NonNull;
 
 public class Barcode extends AppCompatActivity {
-    private CodeScanner mCodeScanner;
+    public EditText teks;
+    public Button barcode;
+    public ImageView image;
+    MultiFormatWriter multiFormatWriter;
+    String teksbarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_barcode);
-//        CodeScannerView scannerView = findViewById(R.id.scanner_view);
-//        mCodeScanner = new CodeScanner(this, scannerView);
-//        mCodeScanner.setDecodeCallback(new DecodeCallback() {
-//            @Override
-//            public void onDecoded(@NonNull final Result result) {
-//                activity.runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//            }
-//        });
-//        scannerView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mCodeScanner.startPreview();
-//            }
-//        });
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mCodeScanner.startPreview();
-    }
+        setContentView(R.layout.activity_barcode);
+        teks = (EditText)findViewById(R.id.edittext);
+        barcode = (Button)findViewById(R.id.barcodeteks);
+        image = (ImageView)findViewById(R.id.imageview);
 
-    @Override
-    protected void onPause() {
-        mCodeScanner.releaseResources();
-        super.onPause();
+        multiFormatWriter = new MultiFormatWriter();
+
+
+
+        barcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teksbarcode = teks.getText().toString();
+                try                {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(teksbarcode, BarcodeFormat.QR_CODE, 300,300);
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    Bitmap bitmap = encoder.createBitmap(bitMatrix);
+                    image.setImageBitmap(bitmap);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
