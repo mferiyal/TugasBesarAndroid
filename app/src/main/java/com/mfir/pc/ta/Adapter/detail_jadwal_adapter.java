e9c2be9a18cb716fa05835389c0a5c60dd30c861
mferiyal <44905071+mferiyal@users.noreply.github.com>
2018-12-28 14:06:35 +0700

    adapter update

--- app/src/main/java/com/mfir/pc/ta/Adapter/detail_jadwal_adapter.java
+++ app/src/main/java/com/mfir/pc/ta/Adapter/detail_jadwal_adapter.java
@@ -9,7 +9,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.mfir.pc.ta.Model.DetailJadwal;
-import com.mfir.pc.ta.R;
+import com.mfir.pc.ta.*;
 
 import java.util.List;
 
